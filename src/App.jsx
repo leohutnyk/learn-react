e7f9a0516b9d1e1c337739f44af5e84cf93a6311
leohutnyk <leohutnyk@gmail.com>
2025-03-09 18:48:49 +0100

    next commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import ContentLoader, { Facebook } from "react-content-loader";
 
 const App = () => {
   const [articles, setArticles] = useState([]);
@@ -14,6 +15,15 @@ const App = () => {
 
     fetchArticles();
   }, []);
+
+  const MyLoader = () => (
+    <ContentLoader viewBox="0 0 380 70">
+      {/* Only SVG shapes */}    
+      <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
+      <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
+      <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
+    </ContentLoader>
+  );
 
   return (
     <div>
